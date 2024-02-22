aa7d217bb40da1e98921a7a7fedc26c9ebe4d20d
BetteriX <szabozsolt430@gmail.com>
2024-02-22 15:03:30 +0100

    1. óra

---
+++ 1.ora/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    char character = 'g';
+    int a = 5;
+    float b = 1.0;
+    double c = 2.0;
+
+    printf("A valtozo erteke: %c\n", character);
+    printf("A valtozo erteke: %d\n", a);
+    printf("A valtozo erteke: %f\n", b);
+    printf("A valtozo erteke: %lf\n", c);
+    */
+
+    printf("Írj be egy szamot!:");
+    int a;
+    scanf("%d", &a);
+
+    printf("A szam: %d\n",a);
+}
---
+++ 1.ora/negyzet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("Adj meg egy szamot: ");
+    int a;
+    scanf("%d",&a);
+
+    printf("A szam: %d\n", a*a);
+    printf("A negyzet kerülete: %d\n", 4*a);
+}
