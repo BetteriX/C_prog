b27e66dc3c7d47c8bf8b665cd886ea0af240bf87
BetteriX <szabozsolt430@gmail.com>
2024-03-11 11:06:50 +0100

    3.ora Hazi Fix

--- Gyakorlat_Hazi/3.ora/gomb.c
+++ Gyakorlat_Hazi/3.ora/gomb.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-#include <math.h>
 #define PI 3.14159265358979323846
+#include <math.h>
 
-int get_double(int s){
-    return s*s;
+int get_double(){
+    double s = 0;
+    printf("Add meg egy valos erteket: ");
+    scanf("%lf", &s);
+
+    return s;
 }
 
-double gomb_felszin(int r){
-    return 4*(r*2*PI);
+double gomb_felszin(double r){
+    return 4* pow(r,2.0) *PI;
 }
 
 double gomb_terfogta(int r){
-    return 4*(r*3*PI*3);
+    return (4* pow(r,3.0) *PI)/3;
 }
 
 
 int main(){
-    int s = 0;
-    printf("Add meg a gomb sugarat: ");
-    scanf("%d", &s);
-
-    int r = get_double(s);
+    double r = get_double();
 
     printf("A gomb felszine: %lf\n", gomb_felszin(r));
     printf("A gomb terfogata: %lf\n", gomb_terfogta(r));
