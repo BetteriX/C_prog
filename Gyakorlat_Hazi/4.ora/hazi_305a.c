ed876540368d44dfef788673a67b3585c55822c9
BetteriX <szabozsolt430@gmail.com>
2024-03-19 10:00:04 +0100

    4.ora felhokarcolo

--- Gyakorlat_Hazi/4.ora/hazi_305a.c
+++ Gyakorlat_Hazi/4.ora/hazi_305a.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int rendezett_e(int n, int tomb[]){
+    if(n = 0){
+        return 1;
+    }
     for(int i = 0; i < n - 1 ; i++){
         // printf("%d > %d\n", tomb[i], tomb[i + 1]);
         if(tomb[i] > tomb[i+1]){
@@ -13,8 +16,9 @@ int rendezett_e(int n, int tomb[]){
 
 int main(){
     // int tomb[10] = {1,2,3,4,5,7,6,8,9,10};
-    int tomb[10] = {1,2,3,4,5,6,7,8,9,10};
-    int meret = 10;
+    //int tomb[10] = {1,2,3,4,5,6,7,8,9,10};
+    int tomb[0];
+    int meret = 0;
 
     if(rendezett_e(meret, tomb) == 0){
         printf("A tömb rendezett!\n");
