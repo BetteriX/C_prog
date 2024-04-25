18ce88d0e8041d58b8c8611bcdf0e2e63f91c40d
BetteriX <szabozsolt430@gmail.com>
2024-04-25 14:51:21 +0200

    zh1_fix

--- ZH/1zh/zh1/Szabo_Zsolt_M5EO1V/feladat4.c
+++ ZH/1zh/zh1/Szabo_Zsolt_M5EO1V/feladat4.c
@@ -4,29 +4,35 @@
 int main(){
     char binaris[100];
     int max_decimal = 0;
-    do  
+    while(1)
     {
         printf("Bináris szám (vége *): ");
         fgets(binaris, 100, stdin);
+        binaris[strlen(binaris)-1]='\0';
 
+        if(strcmp(binaris,"*")==0){
+            break;
+        }
+        else{
         int hossz = strlen(binaris);
-        
+            
         int kettes = 1;
         int decimal = 0;
 
-        for(int i = hossz-2; i >= 0; i--){
+        for(int i = hossz-1; i >= 0; i--){
             if(binaris[i] == '1'){
-                decimal += kettes;
-            }
-            kettes *= 2;
+                    decimal += kettes;
+                }
+        kettes *= 2;
         }
-
         if(max_decimal < decimal){
             max_decimal = decimal;
         }
 
         printf("Decimális szám: %d\n", decimal);
-    } while (binaris[0] != '*');
+        }
+        
+    }
 
     printf("\n");
 
