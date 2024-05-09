95f71344bd1b8d614a3faedfbb5dfe3255f16aa4
BetteriX <szabozsolt430@gmail.com>
2024-05-09 14:05:59 +0200

    fix

--- Gyakorlat_Hazi/11.ora/hattoslotto.c
+++ Gyakorlat_Hazi/11.ora/hattoslotto.c
@@ -3,17 +3,16 @@
 
 #include <stdio.h>
 int main(){
-    int i, j, n, m, k, l;
-    for(i = 1; i <= 25; i++){
-        for(j = i+1; j <= 30; j++){
-            for(n = j+1; n <= 35; n++){
-                for(m = n+1; m <= 40; m++){
-                    for(k = m+1; k <= 42; k++){
-                        for(l = k+1; l <= 45; l++){
+    for(int i = 1; i <= 25; i++){
+        for(int j = i+1; j <= 30; j++){
+            for(int n = j+1; n <= 35; n++){
+                for(int m = n+1; m <= 40; m++){
+                    for(int k = m+1; k <= 42; k++){
+                        for(int l = k+1; l <= 45; l++){
                             int osszeg = i + j + n + m + k + l;
                             int szorzat = i * j * n * m * k * l;
                             if(osszeg == 90 && szorzat == 996300){
-                                printf("%d %d %d %d %d %d", i, j, n, m, k, l);
+                                printf("%d %d %d %d %d %d\n", i, j, n, m, k, l);
                                 return 0;
                             }
                         }
