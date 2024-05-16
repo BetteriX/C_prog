37af9ebe7eace836ceb58e6163ed52b800473534
BetteriX <szabozsolt430@gmail.com>
2024-05-16 13:23:20 +0200

    fix

--- Gyakorlat_Hazi/7.ora/bazinagyszam.c
+++ Gyakorlat_Hazi/7.ora/bazinagyszam.c
@@ -14,7 +14,8 @@ int main(int argc, const char *argv[]){
 
     int sum = 0;
     for(int i = 0; szamok[i] != '\0'; i++){
-        sum += szamok[i] - '0'; // With (- '0') its converts into number    
+        //sum += szamok[i] - '0'; // With (- '0') its converts into number    
+        sum += atoi(&szamok[i]);
     }
 
     printf("\n");
